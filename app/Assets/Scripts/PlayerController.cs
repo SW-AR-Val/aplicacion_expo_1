d1d625962e4630cf516e09b66e6ef39aead5db10
Samuel Alvarez <seam.2194@gmail.com>
2016-04-19 01:41:55 -0430

    Movimiento

---
+++ app/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    GameObject cursor;
+    public float delta = 2f;
+    void Start()
+    {
+        cursor = GameObject.Find("Cursor");
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (Vector3.Distance(pos, cursor.transform.position) > delta)
+        {
+            pos = Vector3.Lerp(pos, cursor.transform.position, 0.04f);
+        }
+        pos.y = transform.position.y;
+        transform.position = pos;
+    }
+}
